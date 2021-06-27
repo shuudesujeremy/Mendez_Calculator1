22ebd0ec19358dfdb0147d82722ba3dc2bb1c5e2
Jeremy Mendez <iamjeremymendez234@gmail.com>
2021-06-27 18:58:21 +0800

    created form

--- Mendez_Calculator1/Form1.Designer.cs
+++ Mendez_Calculator1/Form1.Designer.cs
@@ -29,10 +29,17 @@ namespace Mendez_Calculator1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(501, 509);
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
