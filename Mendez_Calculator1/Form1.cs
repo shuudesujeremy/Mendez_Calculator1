a7fb27643e2cbac484479c74e408c2b1944927f9
Jeremy Mendez <iamjeremymendez234@gmail.com>
2021-06-29 20:54:11 +0800

    added method NumSave to the number buttons

--- Mendez_Calculator1/Form1.cs
+++ Mendez_Calculator1/Form1.cs
@@ -25,52 +25,52 @@ namespace Mendez_Calculator1
 
         private void btn0_Click(object sender, EventArgs e)
         {
-
+            NumSave("0");
         }
 
         private void btn1_Click(object sender, EventArgs e)
         {
-
+            NumSave("1");
         }
 
         private void btn2_Click(object sender, EventArgs e)
         {
-
+            NumSave("2");
         }
 
         private void btn3_Click(object sender, EventArgs e)
         {
-
+            NumSave("3");
         }
 
         private void btn4_Click(object sender, EventArgs e)
         {
-
+            NumSave("4");
         }
 
         private void btn5_Click(object sender, EventArgs e)
         {
-
+            NumSave("5");
         }
 
         private void btn6_Click(object sender, EventArgs e)
         {
-
+            NumSave("6");
         }
 
         private void btn7_Click(object sender, EventArgs e)
         {
-
+            NumSave("7");
         }
 
         private void btn8_Click(object sender, EventArgs e)
         {
-
+            NumSave("8");
         }
 
         private void btn9_Click(object sender, EventArgs e)
         {
-
+            NumSave("9");
         }
 
         private void btnDot_Click(object sender, EventArgs e)
